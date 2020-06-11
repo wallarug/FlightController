285df38113ddc2daa9b3801f57a36fcfe712cff5
wallarug <wallarug@coliemore.com.au>
2020-06-11 22:49:41 +1000

    convert log to linear

---
+++ examples/analog_multiple_unlog.py
@@ -0,0 +1,43 @@
+import time
+import board
+import busio
+from math import log
+
+from digitalio import DigitalInOut, Direction
+from analogio import AnalogIn
+
+spdbrk = AnalogIn(board.SERVO5)
+flaps = AnalogIn(board.SERVO6)
+
+thr1 = AnalogIn(board.RCC1)
+thr2 = AnalogIn(board.RCC2)
+thr3 = AnalogIn(board.RCC3)
+thr4 = AnalogIn(board.RCC4)
+
+axes = [thr1, thr2, thr3, thr4, spdbrk, flaps]
+
+
+def get_voltage(pin):
+    return (pin.value * 3.3) / 65536
+
+def get_percentage(pin):
+    _max = 100
+    _min = 0
+    
+    return (100 - ((pin.value * 3.3 / 65536) / 2.54 * 100))
+
+def get_linear_percentage(pin):
+    return math.log(get_percentage(pin))
+
+report = ""
+
+ 
+while True:
+    report = ""
+
+    for axis in axes:
+        report += str(get_linear_percentage(axis)) + ", "
+
+    print(report)
+
+    time.sleep(0.1)
